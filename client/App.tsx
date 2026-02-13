54036179e789564e69c9151b1a8b6d9385783d93
Pratyush Poudel <pratyushpoudel706@gmail.com>
2026-02-13 10:15:34 -0500

    Added React-Navigation and set the background theme for the project

--- client/App.tsx
+++ client/App.tsx
@@ -1,16 +1,38 @@
 import React from "react";
-// import HomeScreen from "./src/screens/HomeScreen";
-import ConsistencyGraph from "./src/components/ConsistencyGraph";
-import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+import HomeScreen from "./src/screens/HomeScreen";
+
+import ConsistencyGraph from "./src/components/HomeScreen/ConsistencyGraph";
+import CreateProgramBox from "./src/components/HomeScreen/CreateProgramBox";
+import CreateProgramScreen from "./src/screens/CreateProgramScreen";
+import AddANameButton from "./src/components/CreateProgram/AddANameButton";
+import StartEmptyWorkout from "./src/components/HomeScreen/StartEmptyWorkout";
+
+
+const Stack = createNativeStackNavigator();
+
+const MyTheme = {
+  ...DefaultTheme,
+  colors:{
+    ...DefaultTheme.colors,
+    background: '#1c1c20',
+  },
+};
 
 
 export default function App() {
   return (
-    // <HomeScreen/>
     <SafeAreaProvider>
-      <SafeAreaView>
-        <ConsistencyGraph/>
-      </SafeAreaView>
+      <NavigationContainer theme={MyTheme}>
+        <Stack.Navigator screenOptions={{
+          headerShown: false
+        }}>
+          <Stack.Screen name="Home" component={HomeScreen}/>
+        </Stack.Navigator>
+      </NavigationContainer>
     </SafeAreaProvider>
   );
 }
